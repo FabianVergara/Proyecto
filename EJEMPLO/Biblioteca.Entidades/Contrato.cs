f7c2ec487194e2dde16b70baa0fa7ab0c7d3d9ff
kpopse7en <yur.vargas@alumnos.duoc.cl>
2018-04-07 15:14:28 -0300

    ok

---
+++ EJEMPLO/Biblioteca.Entidades/Contrato.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.DALC;
+namespace Biblioteca.Entidades
+{
+    public class Contrato
+    {
+        public int MyProperty { get; set; }
+    }
+}
