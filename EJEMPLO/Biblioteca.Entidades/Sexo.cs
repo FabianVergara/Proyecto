c99c08e1fb8f4d1f138d92b0d68133783556fb66
Fabian <vergarafabianandres@live.com>
2018-04-07 14:10:32 -0400

    lo ultimo en clases

---
+++ EJEMPLO/Biblioteca.Entidades/Sexo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.DALC;//OCUPO LA BIBLIOTECA DE ACCESO A LA bd
+namespace Biblioteca.Entidades
+{
+   public class Sexo
+    {
+        public int IdSexo { get; set; }
+        public String Descripcion { get; set; }
+        Biblioteca.DALC.BeLifeEntities Entidades;//inicaliza
+        public Sexo()
+        {
+            Entidades = new Biblioteca.DALC.BeLifeEntities();//crea la conexion
+        }
+        //metodos
+        
+        
+
+
+
+    }
+}
